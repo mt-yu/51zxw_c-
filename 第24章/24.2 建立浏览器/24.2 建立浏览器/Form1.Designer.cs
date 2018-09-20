6405391e8824de7e5e86123b090048154d502422
mt-yu <30967271+mt-yu@users.noreply.github.com>
2018-09-20 17:54:10 +0800

    2018年9月20日

---
+++ 第24章/24.2 建立浏览器/24.2 建立浏览器/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace _24._2_建立浏览器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tsbBack = new System.Windows.Forms.ToolStripButton();
+            this.tsbForward = new System.Windows.Forms.ToolStripButton();
+            this.tsbRefresh = new System.Windows.Forms.ToolStripButton();
+            this.tsbHome = new System.Windows.Forms.ToolStripButton();
+            this.tsbViewSource = new System.Windows.Forms.ToolStripButton();
+            this.tsbExit = new System.Windows.Forms.ToolStripButton();
+            this.tsbStop = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 97);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(999, 524);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            this.webBrowser1.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser1_NewWindow);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbBack,
+            this.tsbForward,
+            this.tsbRefresh,
+            this.tsbHome,
+            this.tsbStop,
+            this.tsbViewSource,
+            this.tsbExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(999, 39);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "网址：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(769, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(864, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "访问";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tsbBack
+            // 
+            this.tsbBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbBack.Image = global::_24._2_建立浏览器.Properties.Resources.Back;
+            this.tsbBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBack.Name = "tsbBack";
+            this.tsbBack.Size = new System.Drawing.Size(36, 36);
+            this.tsbBack.Text = "toolStripButton1";
+            // 
+            // tsbForward
+            // 
+            this.tsbForward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbForward.Image = global::_24._2_建立浏览器.Properties.Resources.Forward;
+            this.tsbForward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbForward.Name = "tsbForward";
+            this.tsbForward.Size = new System.Drawing.Size(36, 36);
+            this.tsbForward.Text = "toolStripButton2";
+            // 
+            // tsbRefresh
+            // 
+            this.tsbRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbRefresh.Image = global::_24._2_建立浏览器.Properties.Resources.Refresh;
+            this.tsbRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbRefresh.Name = "tsbRefresh";
+            this.tsbRefresh.Size = new System.Drawing.Size(36, 36);
+            this.tsbRefresh.Text = "toolStripButton3";
+            // 
+            // tsbHome
+            // 
+            this.tsbHome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbHome.Image = global::_24._2_建立浏览器.Properties.Resources.Home;
+            this.tsbHome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbHome.Name = "tsbHome";
+            this.tsbHome.Size = new System.Drawing.Size(36, 36);
+            this.tsbHome.Text = "toolStripButton4";
+            // 
+            // tsbViewSource
+            // 
+            this.tsbViewSource.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbViewSource.Image = global::_24._2_建立浏览器.Properties.Resources.Preview;
+            this.tsbViewSource.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbViewSource.Name = "tsbViewSource";
+            this.tsbViewSource.Size = new System.Drawing.Size(36, 36);
+            this.tsbViewSource.Text = "toolStripButton5";
+            // 
+            // tsbExit
+            // 
+            this.tsbExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbExit.Image = global::_24._2_建立浏览器.Properties.Resources.Exit;
+            this.tsbExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbExit.Name = "tsbExit";
+            this.tsbExit.Size = new System.Drawing.Size(36, 36);
+            this.tsbExit.Text = "toolStripButton6";
+            // 
+            // tsbStop
+            // 
+            this.tsbStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbStop.Image = global::_24._2_建立浏览器.Properties.Resources.Stop;
+            this.tsbStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbStop.Name = "tsbStop";
+            this.tsbStop.Size = new System.Drawing.Size(36, 36);
+            this.tsbStop.Text = "toolStripButton1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 621);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripButton tsbBack;
+        private System.Windows.Forms.ToolStripButton tsbForward;
+        private System.Windows.Forms.ToolStripButton tsbRefresh;
+        private System.Windows.Forms.ToolStripButton tsbHome;
+        private System.Windows.Forms.ToolStripButton tsbViewSource;
+        private System.Windows.Forms.ToolStripButton tsbExit;
+        private System.Windows.Forms.ToolStripButton tsbStop;
+    }
+}
+
---
+++ 第28章/28 CRM_System/28 CRM_System/database.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _28_CRM_System
+{
+    class database
+    {
+        SqlConnection con;
+
+        #region 方法
+        /// <summary>
+        /// 打开数据库
+        /// </summary>
+        private void open()
+        { 
+            if(con == null)
+            {
+                string constr = "server=DESKTOP-8BNS2H2\SQLMT; database=CRM_database; user=sa; pwd=asdf12";
+                con = new SqlConnection(const);          
+            }
+            else if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+        /// <summary>
+        /// 数据库关闭
+        /// </summary>
+        private void close()
+        {
+            if(con != null)
+                con.Close();
+        }
+        /// <summary>
+        /// 常见sql命令方法
+        /// </summary>
+        /// <param name="sqlStr"></param>
+        /// <returns></returns>
+        private SqlCommand createcommand(string sqlStr)
+        {
+            open();
+            SqlCommand com = new SqlCommand(sqlStr, con);
+            return com;
+        }
+        /// <summary>
+        /// 执行SQL语句方法
+        /// </summary>
+        /// <param name="sqlStr"></param>
+        public void runSql(string sqlStr)
+        {
+            SqlCommand cmd = createcommand(sqlStr);
+            cmd.ExecuteNonQuery();
+            close();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sqlStr"></param>
+        /// <returns></returns>
+        public int runSqlInt(string sqlStr)
+        {
+            SqlCommand cmd = createcommand(sqlStr);
+            int t = (int) cmd.ExecuteScalar();
+            close();
+            return t;
+        }
+
+        public SqlDataReader runSql(string sqlStr, out SqlDataReader dr)
+        {
+            SqlCommand cmd = createcommand(sqlStr);
+            dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            return dr;
+        }
+
+        public DataTable ConvertDataReaderToDataTable(SqlDataReader datareader)
+        {
+            DataTable dt = new DataTable();
+            DataTable schemaTable = datareader.GetSchemaTable();
+            try 
+	        {	        
+		        foreach(DataRow myRow in schemaTable.Rows)
+                {
+                    DataColumn myDataColumn = new DataColumn();
+                    myDataColumn.DataType = System.Type.GetType("System.String");
+                    dt.Columns.Add(myDataColumn);
+                }
+                while(datareader.Read())
+                {
+                    DataRow mydataRow = dt.NewRow();
+                    for(int i=0; i<schemaTable.Rows.Count; i++)
+                    {
+                        mydataRow[i] = datareader[i].ToString();
+                    }
+                    dt.Rows.Add(mydataRow);
+                    mydataRow = null;
+                }
+                schemaTable = null;
+                datareader.Close();
+                return dt;
+	        }
+	        catch (Exception ex)
+	        {
+                throw new Exception("转换出错",ex);
+	        }
+                }
+
+        #endregion
+    }
+}
