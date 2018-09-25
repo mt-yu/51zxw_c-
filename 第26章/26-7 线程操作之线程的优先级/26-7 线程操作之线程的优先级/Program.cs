d45f4ce4d60f6d288ca81d63ee7b973bec5aa29a
mt-yu <30967271+mt-yu@users.noreply.github.com>
2018-09-25 18:01:39 +0800

    2018年9月25日

---
+++ 第26章/26-7 线程操作之线程的优先级/26-7 线程操作之线程的优先级/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace _26_7_线程操作之线程的优先级
+{
+
+    class Program
+    {
+        public static void method()
+        {
+
+                Console.WriteLine("线程的名称:{0}", Thread.CurrentThread.Name.ToString());
+                Console.WriteLine("线程的优先级:{0}", Thread.CurrentThread.Priority.ToString());
+
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart ts1 = new ThreadStart(method);
+            ThreadStart ts2 = new ThreadStart(method);
+
+            Thread t1 = new Thread(ts1);
+            Thread t2 = new Thread(ts2);
+
+            t1.Name = "t1";
+            t2.Name = "t2";
+
+            t1.Priority = ThreadPriority.Highest;
+            t2.Priority = ThreadPriority.BelowNormal;
+            
+            t1.Start();
+            //Thread.Sleep(5);
+            t2.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 第26章/26.2线程操作之Thread类/26.2线程操作之Thread类/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _26._2线程操作之Thread类
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
